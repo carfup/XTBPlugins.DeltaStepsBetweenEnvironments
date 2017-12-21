367b1929d1241a99bbc4c5757bb1e5f4309b420d
Clement <clement@carfup.com>
2017-12-21 17:52:41 +0100

    Fixcompareenvironmentwhilenosolutionselected (#4)
    * prevent comparing while no solutionname is set
    
    * updated the nuspec

--- DeltaStepsBetweenEnvironments/DeltaStepsBetweenEnvironments.cs
+++ DeltaStepsBetweenEnvironments/DeltaStepsBetweenEnvironments.cs
@@ -19,263 +19,269 @@ using Microsoft.Xrm.Sdk;
 
 namespace Carfup.XTBPlugins.DeltaStepsBetweenEnvironments
 {
-	public partial class DeltaStepsBetweenEnvironments : PluginControlBase, IXrmToolBoxPluginControl, IGitHubPlugin
-	{
-		#region varibables
-		private string[] solutionsList = null;
-		private ConnectionDetail sourceDetail = null;
-		private ConnectionDetail targetDetail = null;
-		IOrganizationService sourceService = null;
-		IOrganizationService targetService = null;
-		List<string> stepsCrmSource = new List<string>();
-		List<string> stepsCrmTarget = new List<string>();
-		private static string solutionPluginStepsName = null;
-		public event EventHandler OnRequestConnection;
-		public string RepositoryName
-		{
-			get
-			{
-				return "XTBPlugins.DeltaStepsBetweenEnvironments";
-			}
-		}
+    public partial class DeltaStepsBetweenEnvironments : PluginControlBase, IXrmToolBoxPluginControl, IGitHubPlugin
+    {
+        #region varibables
+        private string[] solutionsList = null;
+        private ConnectionDetail sourceDetail = null;
+        private ConnectionDetail targetDetail = null;
+        IOrganizationService sourceService = null;
+        IOrganizationService targetService = null;
+        List<string> stepsCrmSource = new List<string>();
+        List<string> stepsCrmTarget = new List<string>();
+        private static string solutionPluginStepsName = null;
+        public event EventHandler OnRequestConnection;
+        public string RepositoryName
+        {
+            get
+            {
+                return "XTBPlugins.DeltaStepsBetweenEnvironments";
+            }
+        }
 
-		public string UserName
-		{
-			get
-			{
-				return "carfup";
-			}
-		}
+        public string UserName
+        {
+            get
+            {
+                return "carfup";
+            }
+        }
 
-		#endregion
+        #endregion
 
-		public DeltaStepsBetweenEnvironments()
-		{
-			InitializeComponent();
+        public DeltaStepsBetweenEnvironments()
+        {
+            InitializeComponent();
 
-			sourceDetail = this.ConnectionDetail;
-			sourceService = this.Service;
+            sourceDetail = this.ConnectionDetail;
+            sourceService = this.Service;
             buttonCompare.Visible = false;
-		}
+        }
 
-		private void toolStripButtonClose_Click(object sender, EventArgs e)
-		{
-			CloseTool();
-		}
+        private void toolStripButtonClose_Click(object sender, EventArgs e)
+        {
+            CloseTool();
+        }
 
-		private void comboBoxSolutionsList_SelectedIndexChanged(object sender, EventArgs e)
-		{
-			if (canProceed())
-			{
-				solutionPluginStepsName = comboBoxSolutionsList.SelectedItem.ToString();
-			}
-		}
+        private void comboBoxSolutionsList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (canProceed())
+            {
+                solutionPluginStepsName = comboBoxSolutionsList.SelectedItem.ToString();
+            }
+        }
 
-		public static List<string> querySteps(IOrganizationService service, List<string> list)
-		{
-			QueryExpression queryExistingAssemblies = new QueryExpression()
-			{
-				EntityName = "solution",
-				ColumnSet = new ColumnSet()
-			};
+        public static List<string> querySteps(IOrganizationService service, List<string> list)
+        {
+            QueryExpression queryExistingAssemblies = new QueryExpression()
+            {
+                EntityName = "solution",
+                ColumnSet = new ColumnSet()
+            };
 
-			queryExistingAssemblies.Criteria.AddCondition("uniquename", ConditionOperator.Equal, solutionPluginStepsName);
-			var crmlocalAssemblyClasses = service.RetrieveMultiple(queryExistingAssemblies).Entities.FirstOrDefault();
+            queryExistingAssemblies.Criteria.AddCondition("uniquename", ConditionOperator.Equal, solutionPluginStepsName);
+            var crmlocalAssemblyClasses = service.RetrieveMultiple(queryExistingAssemblies).Entities.FirstOrDefault();
 
-			QueryExpression queryExistingSteps = new QueryExpression()
-			{
-				EntityName = "solutioncomponent",
-				ColumnSet = new ColumnSet(true)
-			};
+            QueryExpression queryExistingSteps = new QueryExpression()
+            {
+                EntityName = "solutioncomponent",
+                ColumnSet = new ColumnSet(true)
+            };
 
-			queryExistingSteps.Criteria.AddCondition("componenttype", ConditionOperator.Equal, 92);
-			queryExistingSteps.Criteria.AddCondition("solutionid", ConditionOperator.Equal, crmlocalAssemblyClasses.Id);
-			var crmExistingComponentsSteps = service.RetrieveMultiple(queryExistingSteps).Entities;
+            queryExistingSteps.Criteria.AddCondition("componenttype", ConditionOperator.Equal, 92);
+            queryExistingSteps.Criteria.AddCondition("solutionid", ConditionOperator.Equal, crmlocalAssemblyClasses.Id);
+            var crmExistingComponentsSteps = service.RetrieveMultiple(queryExistingSteps).Entities;
 
-			foreach (var step in crmExistingComponentsSteps)
-			{
-				Entity stepdetail = service.Retrieve("sdkmessageprocessingstep", new Guid(step["objectid"].ToString()), new ColumnSet(true));
-				var name = ((EntityReference)stepdetail["eventhandler"]).Name;
-				list.Add(name);
-			}
+            foreach (var step in crmExistingComponentsSteps)
+            {
+                Entity stepdetail = service.Retrieve("sdkmessageprocessingstep", new Guid(step["objectid"].ToString()), new ColumnSet(true));
+                var name = ((EntityReference)stepdetail["eventhandler"]).Name;
+                list.Add(name);
+            }
 
-			return list;
-		}
+            return list;
+        }
 
-		private void comboBoxTargetEnvironmentList_Click(object sender, EventArgs e)
-		{
-			if (OnRequestConnection != null)
-			{
-				var aaa = new RequestConnectionEventArgs
-				{
-					ActionName = "TargetOrganization",
-					Control = this
-				};
-				OnRequestConnection(this, aaa);
-			}
-		}
+        private void comboBoxTargetEnvironmentList_Click(object sender, EventArgs e)
+        {
+            if (OnRequestConnection != null)
+            {
+                var aaa = new RequestConnectionEventArgs
+                {
+                    ActionName = "TargetOrganization",
+                    Control = this
+                };
+                OnRequestConnection(this, aaa);
+            }
+        }
 
-		public void UpdateConnection(IOrganizationService newService, ConnectionDetail connectionDetail, string actionName = "", object parameter = null)
-		{
-			if (actionName == "TargetOrganization")
-			{
-				targetService = newService;
-				targetDetail = connectionDetail;
-				SetConnectionLabel(connectionDetail, "Target");
-			}
-			else
-			{
-				sourceService = newService;
-				sourceDetail = connectionDetail;
-				SetConnectionLabel(connectionDetail, "Source");
-			}
+        public void UpdateConnection(IOrganizationService newService, ConnectionDetail connectionDetail, string actionName = "", object parameter = null)
+        {
+            if (actionName == "TargetOrganization")
+            {
+                targetService = newService;
+                targetDetail = connectionDetail;
+                SetConnectionLabel(connectionDetail, "Target");
+            }
+            else
+            {
+                sourceService = newService;
+                sourceDetail = connectionDetail;
+                SetConnectionLabel(connectionDetail, "Source");
+            }
 
             if (targetService != null && sourceService != null)
                 buttonCompare.Visible = true;
             else
                 buttonCompare.Visible = false;
-		}
-		private void SetConnectionLabel(ConnectionDetail detail, string serviceType)
-		{
-			switch (serviceType)
-			{
-				case "Source":
-					labelSourceEnvironment.Text = detail.ConnectionName;
-					labelSourceEnvironment.ForeColor = Color.Green;
-					break;
+        }
+        private void SetConnectionLabel(ConnectionDetail detail, string serviceType)
+        {
+            switch (serviceType)
+            {
+                case "Source":
+                    labelSourceEnvironment.Text = detail.ConnectionName;
+                    labelSourceEnvironment.ForeColor = Color.Green;
+                    break;
 
-				case "Target":
-					labelTargetEnvironment.Text = detail.ConnectionName;
-					labelTargetEnvironment.ForeColor = Color.Green;
-					break;
-			}
-		}
+                case "Target":
+                    labelTargetEnvironment.Text = detail.ConnectionName;
+                    labelTargetEnvironment.ForeColor = Color.Green;
+                    break;
+            }
+        }
 
-		private void btnChangeTargetEnvironment_Click(object sender, EventArgs e)
-		{
-			if (OnRequestConnection != null)
-			{
-				var arg = new RequestConnectionEventArgs
-				{
-					ActionName = "TargetOrganization",
-					Control = this
-				};
-				OnRequestConnection(this, arg);
-			}
-		}
+        private void btnChangeTargetEnvironment_Click(object sender, EventArgs e)
+        {
+            if (OnRequestConnection != null)
+            {
+                var arg = new RequestConnectionEventArgs
+                {
+                    ActionName = "TargetOrganization",
+                    Control = this
+                };
+                OnRequestConnection(this, arg);
+            }
+        }
 
-		private void buttonCompare_Click(object sender, EventArgs evt)
-		{
-			listBoxSourceTarget.Items.Clear();
-			listBoxTargetSource.Items.Clear();
+        private void buttonCompare_Click(object sender, EventArgs evt)
+        {
+            if (solutionPluginStepsName == null)
+            {
+                MessageBox.Show($"Please select a solution first.");
+                return;
+            }
 
-			string[] diffCrmSourceTarget = null;
-			string[] diffCrmTargetSource = null;
+            listBoxSourceTarget.Items.Clear();
+            listBoxTargetSource.Items.Clear();
 
-			stepsCrmSource.Clear();
-			stepsCrmTarget.Clear();
+            string[] diffCrmSourceTarget = null;
+            string[] diffCrmTargetSource = null;
 
-			WorkAsync(new WorkAsyncInfo
-			{
-				Message = "Comparing the 2 Solutions...",
-				Work = (bw, e) =>
-				{
-					querySteps(sourceService, stepsCrmSource);
-					querySteps(targetService, stepsCrmTarget);
+            stepsCrmSource.Clear();
+            stepsCrmTarget.Clear();
 
-					diffCrmSourceTarget = stepsCrmSource.Except(stepsCrmTarget).ToArray();
-					diffCrmTargetSource = stepsCrmTarget.Except(stepsCrmSource).ToArray();
-				},
-				PostWorkCallBack = e =>
-				{
-					if (e.Error != null)
-					{
-						MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-						return;
-					}
-					if (diffCrmSourceTarget.Count() == 0)
-					{
-						//listBoxSourceTarget.Visible = false;
-						labelSourceTargetMatch.Visible = true;
-					}
-					else
-					{
-						listBoxSourceTarget.Visible = true;
-						listBoxSourceTarget.Items.AddRange(diffCrmSourceTarget);
-						labelSourceTargetMatch.Visible = false;
+            WorkAsync(new WorkAsyncInfo
+            {
+                Message = "Comparing the 2 Solutions...",
+                Work = (bw, e) =>
+                {
+                    querySteps(sourceService, stepsCrmSource);
+                    querySteps(targetService, stepsCrmTarget);
 
-					}
+                    diffCrmSourceTarget = stepsCrmSource.Except(stepsCrmTarget).ToArray();
+                    diffCrmTargetSource = stepsCrmTarget.Except(stepsCrmSource).ToArray();
+                },
+                PostWorkCallBack = e =>
+                {
+                    if (e.Error != null)
+                    {
+                        MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    if (diffCrmSourceTarget.Count() == 0)
+                    {
+                        //listBoxSourceTarget.Visible = false;
+                        labelSourceTargetMatch.Visible = true;
+                    }
+                    else
+                    {
+                        listBoxSourceTarget.Visible = true;
+                        listBoxSourceTarget.Items.AddRange(diffCrmSourceTarget);
+                        labelSourceTargetMatch.Visible = false;
 
-					if (diffCrmTargetSource.Count() == 0)
-					{
-						//listBoxTargetSource.Visible = false;
-						labelTargetSourceMatch.Visible = true;
-					}
-					else
-					{
-						listBoxTargetSource.Items.AddRange(diffCrmTargetSource);
-						listBoxTargetSource.Visible = true;
-						labelTargetSourceMatch.Visible = false;
-					}
-				},
-				ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
-			});
-		}
+                    }
 
-		private void buttonLoadSolutions_Click(object sender, EventArgs evt)
-		{				
-			if(canProceed())
-			{
-				comboBoxSolutionsList.Items.Clear();
-				WorkAsync(new WorkAsyncInfo
-				{
-					Message = "Loading CRM Solutions...",
-					Work = (bw, e) =>
-					{
-						solutionsList = sourceService.RetrieveMultiple(new QueryExpression("solution")
-						{
-							ColumnSet = new ColumnSet("uniquename"),
-						}).Entities.Select(p => p.Attributes["uniquename"].ToString()).OrderBy(p => p).ToArray();
-					},
-					PostWorkCallBack = e =>
-					{
-						if (e.Error != null)
-						{
-							MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-							return;
-						}
+                    if (diffCrmTargetSource.Count() == 0)
+                    {
+                        //listBoxTargetSource.Visible = false;
+                        labelTargetSourceMatch.Visible = true;
+                    }
+                    else
+                    {
+                        listBoxTargetSource.Items.AddRange(diffCrmTargetSource);
+                        listBoxTargetSource.Visible = true;
+                        labelTargetSourceMatch.Visible = false;
+                    }
+                },
+                ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+            });
+        }
 
-						if (solutionsList != null)
-							comboBoxSolutionsList.Items.AddRange(solutionsList);
+        private void buttonLoadSolutions_Click(object sender, EventArgs evt)
+        {
+            if (canProceed())
+            {
+                comboBoxSolutionsList.Items.Clear();
+                WorkAsync(new WorkAsyncInfo
+                {
+                    Message = "Loading CRM Solutions...",
+                    Work = (bw, e) =>
+                    {
+                        solutionsList = sourceService.RetrieveMultiple(new QueryExpression("solution")
+                        {
+                            ColumnSet = new ColumnSet("uniquename"),
+                        }).Entities.Select(p => p.Attributes["uniquename"].ToString()).OrderBy(p => p).ToArray();
+                    },
+                    PostWorkCallBack = e =>
+                    {
+                        if (e.Error != null)
+                        {
+                            MessageBox.Show(this, e.Error.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                            return;
+                        }
 
-					},
-					ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
-				});
-			}
-		}
+                        if (solutionsList != null)
+                            comboBoxSolutionsList.Items.AddRange(solutionsList);
 
-		private void buttonChangeSource_Click(object sender, EventArgs e)
-		{
-			if (OnRequestConnection != null)
-			{
-				var arg = new RequestConnectionEventArgs
-				{
-					ActionName = "SourceOrganization",
-					Control = this
-				};
-				OnRequestConnection(this, arg);
-			}
-		}
+                    },
+                    ProgressChanged = e => { SetWorkingMessage(e.UserState.ToString()); }
+                });
+            }
+        }
+
+        private void buttonChangeSource_Click(object sender, EventArgs e)
+        {
+            if (OnRequestConnection != null)
+            {
+                var arg = new RequestConnectionEventArgs
+                {
+                    ActionName = "SourceOrganization",
+                    Control = this
+                };
+                OnRequestConnection(this, arg);
+            }
+        }
 
 
-		public bool canProceed()
-		{
-			if (sourceService == null || targetService == null)
+        public bool canProceed()
+        {
+            if (sourceService == null || targetService == null)
             {
                 MessageBox.Show("Make sure you are connected to a Source AND Target environments first.");
                 return false;
             }
-			return true;
-		}
-	}
+            return true;
+        }
+    }
 }
